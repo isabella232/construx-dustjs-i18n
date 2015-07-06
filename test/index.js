7acdf0accbf0dcf45693da5197e35f5c95714c0f
Matt Edelman <medelman@paypal.com>
2015-07-06 16:52:55 -0700

    adding more unit tests

--- test/index.js
+++ test/index.js
@@ -24,58 +24,111 @@ var test = require('tap').test,
   path = require('path'),
   Dust = require(path.resolve(__dirname, '..')),
   destRoot = path.resolve(__dirname, 'tmp'),
-  rimraf = require('rimraf'),
   fs = require('fs'),
   i18n = require(path.resolve(__dirname, '../lib/localize'));
 
 
 test('construx-dustjs-i18n', function (t) {
-    var args = {
-        paths: [path.resolve(__dirname, '../tmp/templates')],
-        context: {
-            srcRoot: path.resolve(__dirname, 'fixtures/public'),
-            destRoot: destRoot,
-            filePath: '/templates/localized.js',
-            name: 'localized',
-            ext: 'dust',
-            origName: 'localized'
-        }
-    };
+
 
     t.test('localizes a good dust file', function (t) {
+        var args = {
+            paths: [path.resolve(__dirname, '../tmp/templates')],
+            context: {
+                srcRoot: path.resolve(__dirname, 'fixtures/public'),
+                destRoot: destRoot,
+                filePath: '/templates/localized.js',
+                name: 'localized',
+                ext: 'dust',
+                origName: 'localized',
+                srcPath: path.resolve(__dirname, 'fixtures/public/templates/localized.dust')
+            }
+        };
         var localePath = path.resolve(__dirname, 'fixtures/locales/US/es');
         var dust = Dust({i18n: {contentPath: localePath}});
         t.plan(1);
-        //get good star file
-        fs.readFile(path.resolve(__dirname, 'fixtures/public/templates/localized.dust'), function (err, data) {
-            dust(data, args, function (err, compiled) {
-                t.ok(compiled.indexOf('("<div>  Hola ")') !== -1);
-                t.end();
-            });
-
+        dust(' ', args, function (err, compiled) {
+            t.ok(compiled.indexOf('("<div>  Hola ")') !== -1);
+            t.end();
         });
+
 
     });
 
-    //t.test('processes a bad dust file', function (t) {
-    //    t.plan(1);
-    //    //get bad star file
-    //    fs.readFile(path.resolve(__dirname, 'star/bad.star'), function (err, data) {
-    //        star(data, {paths: '', context: {name: 'star.compiled'}}, function (err, compiled) {
-    //            t.ok(err.name === 'Error');
-    //            t.end();
-    //        });
-    //
-    //    });
-    //
-    //});
+    t.test('processes a bad dust file', function (t) {
+        var args = {
+            paths: [path.resolve(__dirname, '../tmp/templates')],
+            context: {
+                srcRoot: path.resolve(__dirname, 'fixtures/public'),
+                destRoot: destRoot,
+                filePath: '/templates/localized-bad.js',
+                name: 'localized-bad',
+                ext: 'dust',
+                origName: 'localized-bad',
+                srcPath: path.resolve(__dirname, 'fixtures/public/templates/localized-bad.dust')
+            }
+        };
+        var localePath = path.resolve(__dirname, 'fixtures/locales/US/es');
+        var dust = Dust({i18n: {contentPath: localePath}});
+        t.plan(1);
+        dust(' ', args, function (err, compiled) {
+            t.ok(err.name === 'Error');
+            t.end();
+        });
+
+
+    });
+
+    t.test('processes a non-localized dust file', function (t) {
+        var args = {
+            paths: [path.resolve(__dirname, '../tmp/templates')],
+            context: {
+                srcRoot: path.resolve(__dirname, 'fixtures/public'),
+                destRoot: destRoot,
+                filePath: '/templates/index.js',
+                name: 'index',
+                ext: 'dust',
+                origName: 'index',
+                srcPath: path.resolve(__dirname, 'fixtures/public/templates/index.dust')
+            }
+        };
+        var dust = Dust({});
+        t.plan(1);
+        fs.readFile(path.resolve(__dirname, 'fixtures/public/templates/index.dust'), function (err, data) {
+            dust(data, args, function (err, compiled) {
+                t.ok(compiled.indexOf('<h1>Hello, world!</h1>') !== -1);
+                t.end();
+            });
+        });
+
+
+    });
 
 });
+test('construx-dustjs-i18n localize.preHook', function (t) {
 
-//test('teardown', function (t) {
-//    rimraf(destRoot, function (err) {
-//        if (err) {
-//            // don't throw
-//        }
-//    });
-//});
+
+    t.test('correctly constructs context', function (t) {
+        var args = {
+            paths: [path.resolve(__dirname, '../tmp/templates')],
+            context: {
+                srcRoot: path.resolve(__dirname, 'fixtures/public'),
+                destRoot: destRoot,
+                filePath: '/templates/US/fr/localized.js',
+                name: 'US/fr/localized',
+                ext: 'dust'
+            }
+        };
+        i18n.preHook(args.context, function (err, context) {
+            t.plan(3);
+            t.deepEqual(context.locality, {country: 'US', language: 'fr'});
+            t.equal(context.srcPath, path.resolve(__dirname, 'fixtures/public/templates/localized.dust'));
+            t.equal(context.origName, 'localized');
+            t.end();
+        });
+
+
+    });
+
+
+});
